28412da962ecb6fafc8c45dad341ba08bb0aa8c1
YogPanjarale <yog.panjarale@gmail.com>
2021-05-05 19:18:29 +0530

    prediction imports

---
+++ prediction.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+from sklearn.datasets import fetch_openml
+from PIL import Image
+import PIL.ImageOps
