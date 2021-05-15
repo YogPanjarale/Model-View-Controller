a808bb37cbf095a74e99adcd741bf7a2c1314c58
YogPanjarale <yog.panjarale@gmail.com>
2021-05-15 11:23:57 +0530

    i had brokeen it , now working

--- api.py
+++ api.py
@@ -1,15 +1,21 @@
 from flask import Flask,jsonify,request
-from my_classifier import predictImage
+from my_classifier2 import predictImage
+import socket
 app = Flask(__name__)
 
 @app.route("/predict-image",methods = ["POST"])
 def predict():
     image_data = request.files.get("digit")
+    print(request)
     result = predictImage(image_data)
     print(result)
     return jsonify({
         "result":result
     })
-
+@app.route("/hello")
+def hello():
+    return "Hello"
 if __name__== "__main__":
-    app.run()
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+    app.run(host=IPAddr,port=5000)
